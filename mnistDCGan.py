c813ad1f08a4302f33b6d0220d943ef1a6bb7ed0
Max Beckers <mbeckers26@gmail.com>
2021-04-15 10:46:14 +0000

    added comparable discriminator

--- mnistDCGan.py
+++ mnistDCGan.py
@@ -75,6 +75,34 @@ class Discriminator(nn.Module):
         # output = torch.sigmoid(x) # want to predict real or fake so binary output
         return output
 
+class Discriminator2(nn.Module):
+    def __init__(self):
+        super(Discriminator2, self).__init__()
+
+        def discriminator_block(in_filters, out_filters, bn=True):
+            block = [nn.Conv2d(in_filters, out_filters, 3, 2, 1), nn.LeakyReLU(0.2, inplace=True), nn.Dropout2d(0.25)]
+            if bn:
+                block.append(nn.BatchNorm2d(out_filters, 0.8))
+            return block
+
+        self.model = nn.Sequential(
+            *discriminator_block(1, 16, bn=False),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128),
+        )
+
+        # The height and width of downsampled image
+        ds_size = int(np.ceil(28 / 2 ** 4))
+        self.adv_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, 1), nn.Sigmoid())
+
+    def forward(self, img):
+        out = self.model(img)
+        out = out.view(out.shape[0], -1)
+        validity = self.adv_layer(out)
+
+        return validity
+
 def weights_init_normal(m):
     classname = m.__class__.__name__
     if classname.find("Conv") != -1:
@@ -131,7 +159,7 @@ def main():
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
     # instantiate discriminator and it's optimizer and loss
-    discriminator = Discriminator().to(device)
+    discriminator = Discriminator2().to(device)
     discriminator.apply(weights_init_normal)
     D_optimizer = torch.optim.Adam(discriminator.parameters(), lr=args.lr, betas=(args.b1, args.b2))
 
@@ -193,7 +221,7 @@ def main():
                 writer.add_scalar("Train/G_loss", g_loss.item(), iteration_counter)
         
         # for every epoch save generated pic
-        save_image(gen_imgs.data[:10], "images/%d.png" % batch_idx, nrow=5, normalize=True)
+        save_image(gen_imgs.data[:10], "images/%d.png" % epoch, nrow=5, normalize=True)
 
         """
         TEST
